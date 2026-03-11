73094d2723b1413387c5f302196f8ea95a2a98f1
-inf <infgotoinf@gmail.com>
2026-03-11 20:56:10 +0300

    Edit CMakeLists.txt and shell.nix for painless work on nixos. Edit build steps, add build steps for docs in README.md. Create basic Doxyfile. Make minor changes in main/main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -1,13 +1,3 @@
-/* clear.c ... */
-
-/*
- * This example code creates an SDL window and renderer, and then clears the
- * window to a different color every frame, so you'll effectively get a window
- * that's smoothly fading between colors.
- *
- * This code is public domain. Feel free to use it for any purpose!
- */
-
 #define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
@@ -15,6 +5,7 @@
 /* We will use this renderer to draw into this window every frame. */
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
+
 
 /* This function runs once at startup. */
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
@@ -26,7 +17,7 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
         return SDL_APP_FAILURE;
     }
 
-    if (!SDL_CreateWindowAndRenderer("examples/renderer/clear", 640, 480, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+    if (!SDL_CreateWindowAndRenderer("Sandbox", 640, 480, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
         SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
@@ -34,6 +25,7 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 
     return SDL_APP_CONTINUE;  /* carry on with the program! */
 }
+
 
 /* This function runs when a new event (mouse input, keypresses, etc) occurs. */
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
@@ -43,6 +35,7 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
     }
     return SDL_APP_CONTINUE;  /* carry on with the program! */
 }
+
 
 /* This function runs once per frame, and is the heart of the program. */
 SDL_AppResult SDL_AppIterate(void *appstate)
@@ -62,6 +55,7 @@ SDL_AppResult SDL_AppIterate(void *appstate)
 
     return SDL_APP_CONTINUE;  /* carry on with the program! */
 }
+
 
 /* This function runs once at shutdown. */
 void SDL_AppQuit(void *appstate, SDL_AppResult result)
