cd2abe8f9cf8b16b11a04e68a6e33a53d6b3fbf2
U-TP\studentcoll <studentcoll@9pc2.tp.local>
2026-03-18 09:18:29 +0300

    Remove accidentely added build directory with all of it's contents

--- build/vendored/SDL_ttf/external/plutovg-build/CMakeFiles/plutovg.dir/source/plutovg-ft-math.c.o.d
+++
@@ -1,4 +0,0 @@
-vendored/SDL_ttf/external/plutovg-build/CMakeFiles/plutovg.dir/source/plutovg-ft-math.c.o: \
- C:/msys64/home/studentcoll/Sand/vendored/SDL_ttf/external/plutovg/source/plutovg-ft-math.c \
- C:/msys64/home/studentcoll/Sand/vendored/SDL_ttf/external/plutovg/source/plutovg-ft-math.h \
- C:/msys64/home/studentcoll/Sand/vendored/SDL_ttf/external/plutovg/source/plutovg-ft-types.h
